df1282c51894365038aeede5bad15c59c9d897e5
Rajib <rajibaqil11@gmail.com>
2026-02-08 06:29:19 +0700

    fix: utc to local time

--- src/controllers/prayer.controller.ts
+++ src/controllers/prayer.controller.ts
@@ -1,25 +1,13 @@
 import type { Context } from "hono";
 import { getPrayerTimesFromEquran } from "../services/equranShalat.service";
-
-function todayJakartaISO(): string {
-  return new Intl.DateTimeFormat("en-CA", {
-    timeZone: "Asia/Jakarta",
-  }).format(new Date());
-}
-
-function assertDate(dateStr: string) {
-  if (!/^\d{4}-\d{2}-\d{2}$/.test(dateStr)) {
-    throw new Error('Invalid "date". Use format YYYY-MM-DD');
-  }
-}
+import { parseDateParam } from "../utils/date.ts";
 
 export async function getPrayerTimes(c: Context) {
   // ✅ default values
   const provinsi = c.req.query("provinsi") ?? "DKI Jakarta";
   const kabkota = c.req.query("kabkota") ?? "Kota Jakarta";
-  const date = c.req.query("date") ?? todayJakartaISO();
-
-  assertDate(date);
+  const dateMoment = parseDateParam(c.req.query("date"));
+  const date = dateMoment.format("YYYY-MM-DD");
 
   const data = await getPrayerTimesFromEquran({
     provinsi,
