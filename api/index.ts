881f4bfe4cf08adc7360978e9f49d75f6ee5ec5b
Rajib <rajibaqil11@gmail.com>
2026-02-08 07:06:56 +0700

    fix: cannot find module

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,17 @@
-import app from "./app";
+import { Hono } from "hono";
+import { hijriRoute } from "./routes/hijri.route";
+import { prayerRoute } from "./routes/prayer.route";
 
+export const app = new Hono();
+
+app.get("/", (c) => c.json({ ok: true }));
+
+app.route("/", hijriRoute);
+app.route("/", prayerRoute);
+
+app.onError((err, c) => {
+  return c.json({ status: "error", message: err.message }, 400);
+});
+
+// untuk Vercel: default export app
 export default app;
